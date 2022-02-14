3673e1f3d9a889ac851a0d0c8287df7ed5d2af17
longjianjiang <616393956@qq.com>
2022-02-14 20:43:28 +0800

    Update

--- popup.js
+++ popup.js
@@ -1,30 +1,3 @@
-// let changeColor = document.getElementById("changeColor");
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
-
-// function setPageBackgroundColor() {
-//   console.log("setPageBackgroundColor");
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     console.log("setPageBackgroundColor1");
-//     document.body.style.backgroundColor = color;
-
-//     var links = document.getElementsByTagName('a');
-//     for (var i=0, len=links.length; i < len; i++) {
-//       links[i].target = '_blank';
-//     }
-//   });
-// }
 
 let isATagOpenNewTab = document.getElementById("isATagOpenNewTab");
 var originalLinkTargets = {};
