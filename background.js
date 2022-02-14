4ffdc2e7d2ce6041a444ab189b63b002d6fb13ac
longjianjiang <616393956@qq.com>
2022-02-14 21:47:29 +0800

    Update

--- background.js
+++ background.js
@@ -2,20 +2,14 @@ chrome.runtime.onInstalled.addListener(() => {
   // do nothing.
 });
 
-function firstOpenATag() {
-  var links = document.getElementsByTagName('a');
-  for (var i=0, len=links.length; i < len; i++) {
-    links[i].target = "_blank";
-  }
-}
-
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     let url = new URL(tab.url);
     let host = url.hostname;
-  
+
     chrome.storage.sync.get(host, (res) => {
       if (res[host] == true) {
+        console.log("do change.");
         chrome.scripting.executeScript({
           target: { tabId: tabId },
           function: firstOpenATag,
@@ -23,4 +17,11 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
       }
     });
   }
-})
+});
+
+function firstOpenATag() {
+  var links = document.getElementsByTagName('a');
+  for (var i=0, len=links.length; i < len; i++) {
+    links[i].target = "_blank";
+  }
+}
