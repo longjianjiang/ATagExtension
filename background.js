4d77eac1b3f3df993c485cad095cb58b825085f6
jianglongjian <longjian.jiang@ximalaya.com>
2022-02-14 16:45:03 +0800

    Update

---
+++ background.js
@@ -0,0 +1,26 @@
+chrome.runtime.onInstalled.addListener(() => {
+  // do nothing.
+});
+
+function firstOpenATag() {
+  var links = document.getElementsByTagName('a');
+  for (var i=0, len=links.length; i < len; i++) {
+    links[i].target = "_blank";
+  }
+}
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    let url = new URL(tab.url);
+    let host = url.hostname;
+  
+    chrome.storage.sync.get(host, (res) => {
+      if (res[host] == true) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          function: firstOpenATag,
+        });
+      }
+    });
+  }
+})
---
+++ popup.js
@@ -0,0 +1,105 @@
+// let changeColor = document.getElementById("changeColor");
+
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
+
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageBackgroundColor,
+//   });
+// });
+
+// function setPageBackgroundColor() {
+//   console.log("setPageBackgroundColor");
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     console.log("setPageBackgroundColor1");
+//     document.body.style.backgroundColor = color;
+
+//     var links = document.getElementsByTagName('a');
+//     for (var i=0, len=links.length; i < len; i++) {
+//       links[i].target = '_blank';
+//     }
+//   });
+// }
+
+let isATagOpenNewTab = document.getElementById("isATagOpenNewTab");
+var originalLinkTargets = {};
+
+chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+  let url = new URL(tabs[0].url);
+  let host = url.hostname;
+
+  chrome.storage.sync.get(host, (res) => {
+    console.log(res);
+    if (res) {
+      isATagOpenNewTab.checked = res[host];
+      if (res[host] == true) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: firstOpenATag,
+        });
+      }
+    } else {
+      isATagOpenNewTab = false;
+    }
+  });
+});
+
+isATagOpenNewTab.addEventListener('change', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (isATagOpenNewTab.checked) {
+    console.log("Checkbox is checked..");
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: openATagExtension,
+    });
+
+  } else {
+    console.log("Checkbox is not checked..");
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: closeATagExtension,
+    });
+  }
+});
+
+function openATagExtension() {
+  let url = new URL(location.href);
+  let host = url.hostname;
+  var obj = {};
+  obj[host] = true;
+  chrome.storage.sync.set(obj);
+
+  var links = document.getElementsByTagName('a');
+  for (var i=0, len=links.length; i < len; i++) {
+    links[i].target = "_blank";
+  }
+}
+
+function closeATagExtension() {
+  let url = new URL(location.href);
+  let host = url.hostname;
+  var obj = {};
+  obj[host] = false;
+  chrome.storage.sync.set(obj);
+
+  var links = document.getElementsByTagName('a');
+  for (var i=0, len=links.length; i < len; i++) {
+    links[i].target = "_self";
+  }
+}
+
+function firstOpenATag() {
+  var links = document.getElementsByTagName('a');
+  for (var i=0, len=links.length; i < len; i++) {
+    links[i].target = "_blank";
+  }
+}
+
